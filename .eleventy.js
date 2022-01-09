595c5897a9d815cde04fa04dbe908231ef0da088
noboru-kudo <noboru-kudo@mamezou.com>
2022-01-09 16:52:08 +0900

    Add category to ogp

--- .eleventy.js
+++ .eleventy.js
@@ -104,7 +104,6 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addFilter('blogPage', (fileSlug) => {
-    console.log(fileSlug)
     if (/^[0-9]{4}_.*$/.test(fileSlug)) {
       return fileSlug.substring(5);
     }

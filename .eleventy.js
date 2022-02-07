22e85951f1e5b02910781415db90b7091a210fdd
noboru kudoh <noboru-kudo@mamezou.com>
2022-02-07 10:33:10 +0900

    Introduce markdown-it-container plugin (#67)
    * Introduce markdown-it-container plugin
    
    * Add OGP images for social media
    
    Co-authored-by: github-actions <github-actions@mamezou.com>

--- .eleventy.js
+++ .eleventy.js
@@ -7,18 +7,49 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const markdownItFootNote = require("markdown-it-footnote");
-const markdownItTableOfContents = require("markdown-it-table-of-contents")
+const markdownItTableOfContents = require("markdown-it-table-of-contents");
+const markdownItContainer = require("markdown-it-container");
 const packageVersion = require("./package.json").version;
 const codeClipboard = require("eleventy-plugin-code-clipboard");
-// const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 
 // for Node.js 14
 String.prototype.replaceAll = function (from, to) {
-  return this.replace(new RegExp(from, "g"), to)
+  return this.replace(new RegExp(from, "g"), to);
 };
 
+const icons = {
+  info: '<!-- <%= octicon "info" %> --><svg class="octicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20"><path fill-rule="evenodd" d="M8 1.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13zM0 8a8 8 0 1116 0A8 8 0 010 8zm6.5-.25A.75.75 0 017.25 7h1a.75.75 0 01.75.75v2.75h.25a.75.75 0 010 1.5h-2a.75.75 0 010-1.5h.25v-2h-.25a.75.75 0 01-.75-.75zM8 6a1 1 0 100-2 1 1 0 000 2z"></path></svg>',
+  alert: '<!-- <%= octicon "alert" %> --><svg class="octicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20"><path fill-rule="evenodd" d="M8.22 1.754a.25.25 0 00-.44 0L1.698 13.132a.25.25 0 00.22.368h12.164a.25.25 0 00.22-.368L8.22 1.754zm-1.763-.707c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0114.082 15H1.918a1.75 1.75 0 01-1.543-2.575L6.457 1.047zM9 11a1 1 0 11-2 0 1 1 0 012 0zm-.25-5.25a.75.75 0 00-1.5 0v2.5a.75.75 0 001.5 0v-2.5z"></path></svg>',
+  stop: '<!-- <%= octicon "stop" %> --><svg class="octicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20"><path fill-rule="evenodd" d="M4.47.22A.75.75 0 015 0h6a.75.75 0 01.53.22l4.25 4.25c.141.14.22.331.22.53v6a.75.75 0 01-.22.53l-4.25 4.25A.75.75 0 0111 16H5a.75.75 0 01-.53-.22L.22 11.53A.75.75 0 010 11V5a.75.75 0 01.22-.53L4.47.22zm.84 1.28L1.5 5.31v5.38l3.81 3.81h5.38l3.81-3.81V5.31L10.69 1.5H5.31zM8 4a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 018 4zm0 8a1 1 0 100-2 1 1 0 000 2z"></path></svg>',
+  check: '<!-- <%= octicon "check" %> --><svg class="octicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" width="20" height="20"><path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>',
+};
+
+const containerOptions = {
+  validate: (params) => (/^(info|alert|stop|check)\s*.*$/.test(params.trim())),
+  render: (tokens, idx) => {
+    const expression = tokens[idx].info.trim();
+    if (tokens[idx].nesting === 1) {
+      const style = 'font-size:1.2em;min-height:30px;display:inline-flex;align-items:center;';
+      switch (expression) {
+        case "info":
+          return `<div class="flash"><span style="${style}">${icons.info} Information</span>`;
+        case "alert":
+          return `<div class="flash flash-warn"><span style="${style}">${icons.alert} Warning</span>`;
+        case "stop":
+          return `<div class="flash flash-error"><span style="${style}">${icons.stop} Warning</span>`;
+        case "check":
+          return `<div class="flash flash-success"><span style="${style}">${icons.check} Information</span>`;
+        default:
+          return '<div class="flash">'
+      }
+    } else {
+      return '</div>\n';
+    }
+  }
+};
+
+
 module.exports = function (eleventyConfig) {
-  // eleventyConfig.addPlugin(UpgradeHelper);
   eleventyConfig.addPlugin(socialImages);
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
@@ -189,7 +220,10 @@ module.exports = function (eleventyConfig) {
       containerClass: "post__toc",
       containerHeaderHtml: '<div class="toc-container-header"><p>Contents</p></div>'
     })
-    .use(codeClipboard.markdownItCopyButton);
+    .use(codeClipboard.markdownItCopyButton, {
+      buttonClass: "tdbc-copy-button"
+    })
+    .use(markdownItContainer, "flash", containerOptions);
 
   eleventyConfig.setLibrary("md", markdownLibrary);
 

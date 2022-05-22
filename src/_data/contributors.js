89c47ec8a761032f773fa9784a1359075ce87843
noboru kudoh <noboru-kudo@mamezou.com>
2022-05-22 19:26:43 +0900

    Add author list link (#141)
    * Add author list link
    
    * Add OGP images for social media
    
    * remove debug code
    
    * Add OGP images for social media
    
    Co-authored-by: github-actions <github-actions@mamezou.com>

---
+++ src/_data/contributors.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "noboru-kudo": "kudoh",
+  "masahiro-kondo": "kondoumh",
+  "shigeki-shoji": "edward-mamezou",
+  "toshio-ogiwara": "ogiwarat",
+  "satoshi-kawasaki": "satoshi-kawasaki",
+  "yoshihisa-muta": "ymuta",
+  "shuichi-takatsu": "shuichi-takatsu",
+  "nozomu-tanaka": "nzm-tnk"
+};

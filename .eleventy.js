be9460e1b6a2aebc2297f3d9c7ed0b0728f18c09
noboru-kudo <noboru-kudo@mamezou.com>
2022-07-31 23:14:34 +0900

    add monthly summary page(for internal)

--- .eleventy.js
+++ .eleventy.js
@@ -85,6 +85,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('byAuthor',
     (contributorArticles, author) => contributorArticles.filter(contributor => contributor.name === author));
   eleventyConfig.addFilter('selectAuthor', (hrefs, author) => hrefs.filter(href => href.includes(author)));
+  eleventyConfig.addCollection('currentMonthPosts', (collection) => getPosts(collection).filter(post => post.date.getMonth() === new Date().getMonth()));
   eleventyConfig.addCollection('articles', getPosts);
   eleventyConfig.addCollection('tagList', require("./11ty/tag-list"));
   eleventyConfig.addCollection('contributorArticles', require("./11ty/contributor-articles"));

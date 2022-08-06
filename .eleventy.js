849a31a60c47482ba573e0292c37bcc9b7623fa3
noboru-kudo <noboru-kudo@mamezou.com>
2022-08-06 21:27:37 +0900

    add netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -62,6 +62,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/previews");
   eleventyConfig.addPassthroughCopy({"./node_modules/photoswipe/dist": "photoswipe"});
+  eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
   eleventyConfig.addShortcode("packageVersion", () => `v${packageVersion}`);

aece444ec6a6257df3ef047e54f4650bfbfa8401
noboru-kudo <noboru-kudo@mamezou.com>
2023-01-04 09:46:49 +0900

    fix: markdown-it custom rule to 4.x

--- eleventy.cjs
+++ eleventy.cjs
@@ -171,11 +171,12 @@ module.exports = function (eleventyConfig) {
       md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
         if (isInternalLink(tokens[idx])) {
           // skip internal link
-          return self.renderToken(tokens, idx, options, env, self);
+          return self.renderToken(tokens, idx, options);
         }
-        tokens[idx].attrPush(["target", "_blank"], ["rel", "noopener noreferrer"]);
+        tokens[idx].attrPush(["target", "_blank"]);
+        tokens[idx].attrPush(["rel", "noopener noreferrer"]);
         tokens[idx].attrJoin("class", "new-tab-link");
-        return self.renderToken(tokens, idx, options, env, self);
+        return self.renderToken(tokens, idx, options);
       };
     });
 

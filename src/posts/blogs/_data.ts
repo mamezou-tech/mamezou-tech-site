ba645d04841257dbaa947c3b9d75e4c79b7ddd13
noboru-kudo <noboru-kudo@mamezou.com>
2023-10-01 15:07:32 +0900

    feat: dynamic og image generation on edge

--- src/posts/blogs/_data.ts
+++ src/posts/blogs/_data.ts
@@ -7,6 +7,7 @@ const dropPrefix = (fileSlug: string) =>
 const data = {
   pageNavigationTag: 'posts',
   tags: ['posts'],
+  category: 'ブログ',
   url: (page: Page) => {
     const dt = DateTime.fromJSDate(page.data.date);
     const fileName = dropPrefix(page.src.slug);

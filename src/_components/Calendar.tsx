4859a31e220435c7597dcc498de4f6c9a119dbc3
Masahiro Kondou <kondoumh@gmail.com>
2023-11-21 08:45:51 +0900

    feat: advent calendar components (#916)
    * wip: add calendar component
    
    * use data file
    
    * parameterize the data file
    
    * update dir tree
    
    * fix: typo

---
+++ src/_components/Calendar.tsx
@@ -0,0 +1,58 @@
+const Entry = ({ dayOfWeek, date, author, githubUser, title, url }) => {
+	const authorImageURL = githubUser ? `https://github.com/${githubUser}.png` : null;
+	const authorLink = author ? `/authors/${author}` : null;
+	return (
+		<div style={{ border: '1px solid #ddd', padding: '6px', margin: '3px', borderRadius: '10px' }}>
+ 			<p style={{ textAlign: 'center' }}>{dayOfWeek}</p>
+			<p style={{ textAlign: 'center' }}>{date}</p>
+      <p style={{fontSize: '14px'}}>
+				{authorImageURL && 
+        <a href={authorLink}><img src={authorImageURL} alt={`Author: ${author}`} style={{ width: '30px', height: '30px', borderRadius: '50%', marginRight: '8px' }} /></a>}
+        {author ? (
+        <a href={authorLink}>{author}</a>
+      ) : (
+        author
+      )}</p>
+			<p style={{ textAlign: 'left', fontSize: '14px'}}>
+				{url ? (
+					<a href={url}>{title}</a>
+				) : (
+					title
+				)}
+			</p>
+		</div>
+	);
+};
+
+export default ({ year, weekend, events }) => {
+	const event = events.advent[year];
+	const repeat = weekend ? 7 : 5;
+	return (
+		<div>
+			<style>
+				{`
+					.grid-container {
+						display: grid;
+						grid-template-columns: repeat(${repeat}, 1fr);
+						gap: 1px;
+					}
+
+					@media (max-width: 600px) {
+						.grid-header {
+							display: none;
+						}
+
+						.grid-container {
+							grid-template-columns: repeat(1, 1fr);
+						}
+					}
+				`}
+			</style>
+			<div className="grid-container">
+				{event.entries.map((entry, index) => (
+					<Entry key={index} dayOfWeek={entry.dayOfWeek} date={entry.date} author={entry.author} githubUser={entry.githubUser} title={entry.title} url={entry.url} />
+				))}
+			</div>
+		</div>
+	);
+};
--- src/events/advent/_data.ts
+++ src/events/advent/_data.ts
@@ -1,7 +1,7 @@
 import { Page } from 'lume/core/filesystem.ts';
 
 const data = {
-    layout: 'layouts/event.njk',
+    layout: 'layouts/advent.njk',
     category: 'event',
     url: (page: Page) => `/events/advent-calendar/${page.src.slug}/`
 };

70aae543aa197f64c05848979f5a00d3368b542b
Masahiro Kondo <kondoumh@gmail.com>
2023-11-20 23:40:25 +0900

    parameterize the data file

--- src/_components/Calendar.tsx
+++ src/_components/Calendar.tsx
@@ -1,21 +1,3 @@
-import { PageData } from "lume/core.ts";
-
-interface Props extends PageData {
-	// from _data/events/advent2022.json
-	events: {
-		advent2022: {
-			entries: {
-				dayOfWeek: string;
-				date: string;
-				author: string;
-				githubUser: string;
-				title: string;
-				url: string;
-			}[];
-		}
-	};
-}
-
 const Entry = ({ dayOfWeek, date, author, githubUser, title, url }) => {
 	const authorImageURL = githubUser ? `https://github.com/${githubUser}.png` : null;
 	const authurLink = author ? `/authors/${author}` : null;
@@ -42,31 +24,35 @@ const Entry = ({ dayOfWeek, date, author, githubUser, title, url }) => {
 	);
 };
 
-export default ({ events }: Props) => (
-	<div>
-		<style>
-			{`
-				.grid-container {
-					display: grid;
-					grid-template-columns: repeat(7, 1fr);
-					gap: 1px;
-				}
-
-				@media (max-width: 600px) {
-					.grid-header {
-						display: none;
-					}
-
+export default ({ year, weekend, events }) => {
+	const event = events["advent"+year];
+	const repeat = weekend ? 7 : 5;
+	return (
+		<div>
+			<style>
+				{`
 					.grid-container {
-						grid-template-columns: repeat(1, 1fr);
+						display: grid;
+						grid-template-columns: repeat(${repeat}, 1fr);
+						gap: 1px;
 					}
-				}
-			`}
-		</style>
-		<div className="grid-container">
-			{events.advent2022.entries.map((entry, index) => (
-				<Entry key={index} dayOfWeek={entry.dayOfWeek} date={entry.date} author={entry.author} githubUser={entry.githubUser} title={entry.title} url={entry.url} />
-			))}
+
+					@media (max-width: 600px) {
+						.grid-header {
+							display: none;
+						}
+
+						.grid-container {
+							grid-template-columns: repeat(1, 1fr);
+						}
+					}
+				`}
+			</style>
+			<div className="grid-container">
+				{event.entries.map((entry, index) => (
+					<Entry key={index} dayOfWeek={entry.dayOfWeek} date={entry.date} author={entry.author} githubUser={entry.githubUser} title={entry.title} url={entry.url} />
+				))}
+			</div>
 		</div>
-	</div>
-);
+	);
+};

dbc7d9118e96c74501d4c2deaa1c59a54b21be10
noboru-kudo <noboru-kudo@mamezou.com>
2023-12-13 17:04:23 +0900

    fix: Refactor variables and enhance function in layout files
    The 'post.njk' layout has been updated to use more straightforward variable names 'date' and 'url', replacing 'page.date' and 'page.url'. Additionally, the 'shortDesc' function within 'short_desc.ts' has been enhanced to handle both 'Lume.Data' and 'Lume.Page' input, improving code flexibility and robustness.

--- lume/filters/short_desc.ts
+++ lume/filters/short_desc.ts
@@ -1,18 +1,27 @@
 import { chop } from "./utils.ts";
 
+function isPage(target: Lume.Data | Lume.Page): target is Lume.Page {
+  return "src" in target
+}
 export const shortDesc = (
   pages: Lume.Data[],
-  pageData: Lume.Data,
+  pageData: Lume.Data | Lume.Page,
   defaultValue: string,
 ) => {
-  const path = pageData.page?.src.path;
-  if (!path) return defaultValue;
+  const [path, url] = isPage(pageData) ? [pageData.src.path, pageData.data.url] : [pageData.page.src.path, pageData.url];
+  if (!path) {
+    return defaultValue
+  }
 
   const isPost = path.includes("/posts/");
-  if (!isPost) return defaultValue;
+  if (!isPost) {
+    return defaultValue;
+  }
 
-  const post = pages.find((el) => el.url === pageData.url);
-  if (!post) return defaultValue;
+  const post = pages.find((el) => el.url === url);
+  if (!post) {
+    return defaultValue;
+  }
   const content = post.children?.toString()
     .replace(/(<([^>]+)>)/gi, "")
     .replace(/[\r\n]/gi, "");

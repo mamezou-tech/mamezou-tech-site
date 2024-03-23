d70f657c5309623e9fe922ef585d67670315b3b4
Masahiro Kondo <kondoumh@gmail.com>
2024-03-23 18:54:56 +0900

    fix: build error

--- src/posts/robotics/beanus/_data.ts
+++
@@ -1,8 +0,0 @@
-import { Page } from 'lume/core/filesystem.ts';
-
-const data = {
-  category: 'ロボット - Beanus',
-  url: (page: Page) => `/robotics/beanus/${page.src.slug}/`
-};
-
-export default data;

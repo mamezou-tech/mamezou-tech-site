0f1307797a09f335ae5f7fee421d9df29d5d2554
noboru-kudo <noboru-kudo@mamezou.com>
2024-11-17 09:52:00 +0900

    Add AdventBanner2024 component and update content
    Replaced SummerBanner2024 with AdventBanner2024 in multiple templates to feature the new event. Updated styling and content to reflect the Advent Calendar 2024 promotion.

---
+++ src/_components/AdventBanner2024.tsx
@@ -0,0 +1,24 @@
+import EventBanner from "./EventBanner.tsx";
+
+export default () => {
+  const content = (
+    <>
+      一年を締めくくる特別なイベント、アドベントカレンダーを今年も開催します！<br />
+      初心者からベテランまで楽しめる内容で、毎日新しい技術トピックをお届けします。<br />
+      詳細は<a href="/events/advent-calendar/2024/">こちら</a>から！
+    </>
+  );
+  return (
+    <EventBanner
+      title="アドベントカレンダー2024開催します(12/1~12/25)！"
+      lpLink="/events/advent-calendar/2024/"
+      image={{
+        src: "/img/event/2024-advent-100.webp",
+        width: 100,
+        height: 100,
+      }}
+    >
+      {content}
+    </EventBanner>
+  );
+};

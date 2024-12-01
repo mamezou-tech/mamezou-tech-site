687951718966048a0c4c9f97964871ab9f3b8505
noboru-kudo <noboru-kudo@mamezou.com>
2024-12-02 08:57:39 +0900

    chore: advent start

--- src/_components/AdventBanner2024.tsx
+++ src/_components/AdventBanner2024.tsx
@@ -3,14 +3,14 @@ import EventBanner from "./EventBanner.tsx";
 export default () => {
   const content = (
     <>
-      一年を締めくくる特別なイベント、アドベントカレンダーを今年も開催します！<br />
+      一年を締めくくる特別なイベント、アドベントカレンダーを今年も開催しています！<br />
       初心者からベテランまで楽しめる内容で、毎日新しい技術トピックをお届けします。<br />
       詳細は<a href="/events/advent-calendar/2024/">こちら</a>から！
     </>
   );
   return (
     <EventBanner
-      title="アドベントカレンダー2024開催します(12/1~12/25)！"
+      title="アドベントカレンダー2024開催中！"
       lpLink="/events/advent-calendar/2024/"
       image={{
         src: "/img/event/2024-advent-100.webp",

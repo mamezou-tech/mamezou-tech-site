be510f851c7f965c43376d4a229caa60f129df95
noboru-kudo <noboru-kudo@mamezou.com>
2025-01-04 18:48:54 +0900

    chore: ensure mermaid is initialized for iPadOS
    Simplify Mermaid initialization in configuration.
    
    Replaced the asynchronous Mermaid initialization with a simpler direct import and initialization. Removed unnecessary icon pack registration and tag-specific handling to streamline the script. This improves readability and reduces complexity in the configuration.

--- _config.ts
+++ _config.ts
@@ -255,16 +255,8 @@ site.helper(
   "mermaidTag",
   () =>
     `<script type="module">
-const initializeMermaid = async () => {
-  const mermaid = (await import("https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs")).default;
-  mermaid.initialize({startOnLoad:true})
-  mermaid.registerIconPacks([ 
-    { name: 'logos', loader: () => fetch('https://unpkg.com/@iconify-json/logos@1.2.0/icons.json').then((res) => res.json())},
-    { name: 'mdi', loader: () => fetch('https://unpkg.com/@iconify-json/mdi@1.2.0/icons.json').then((res) => res.json())}
-  ])
-};
-const tag = window.document.querySelector('pre.mermaid');
-if (tag) initializeMermaid()
+import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs";
+mermaid.initialize();
 </script>`,
   { type: "tag" },
 );
